cfd63e59783e826e8681fd326a526e6562b481da
unknown <asko2212@hotmail.com>
2021-09-03 14:43:59 +0200

    object array

--- main.js
+++ main.js
@@ -1,107 +1,107 @@
 //ACCORDION
 
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName('accordion');
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+    acc[i].addEventListener('click', function () {
+        this.classList.toggle('active');
 
-    var icon = this.childNodes[1].childNodes[3];
-    icon.classList.toggle("rotate-icon");
+        var icon = this.childNodes[1].childNodes[3];
+        icon.classList.toggle('rotate-icon');
 
-    // console.log(this.childNodes[1].childNodes[3]);
+        // console.log(this.childNodes[1].childNodes[3]);
 
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+        var panel = this.nextElementSibling;
+        if (panel.style.display === 'block') {
+            panel.style.display = 'none';
+        } else {
+            panel.style.display = 'block';
+        }
+    });
 }
 
 //COUNTDOWN TIMER
 
 let sec = 1800;
-let count = document.getElementById("timer");
-let text = document.getElementById("text-timer");
+let count = document.getElementById('timer');
+let text = document.getElementById('text-timer');
 let secPass;
 let countDown = setInterval(() => {
-  "use strict";
-  secpass();
+    'use strict';
+    secpass();
 }, 1000);
 
 function secpass() {
-  "use strict";
+    'use strict';
 
-  let min = Math.floor(sec / 60),
-    remSec = sec % 60;
+    let min = Math.floor(sec / 60),
+        remSec = sec % 60;
 
-  if (remSec < 10) {
-    remSec = "0" + remSec;
-  }
-  if (min < 10) {
-    min = "0" + min;
-  }
-  count.innerHTML = min + ":" + remSec;
+    if (remSec < 10) {
+        remSec = '0' + remSec;
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+    count.innerHTML = min + ':' + remSec;
 
-  if (sec > 0) {
-    sec = sec - 1;
-  } else {
-    clearInterval(countDown);
+    if (sec > 0) {
+        sec = sec - 1;
+    } else {
+        clearInterval(countDown);
 
-    count.innerHTML = "AKCIJA ZAVRŠILA";
-    text.style.display = "none";
-  }
+        count.innerHTML = 'AKCIJA ZAVRŠILA';
+        text.style.display = 'none';
+    }
 }
 
 // SMOOTH SCROLL
 $(document).ready(function () {
-  $("a").on("click", function (event) {
-    if (this.hash !== "") {
-      event.preventDefault();
+    $('a').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
 
-      var hash = this.hash;
+            var hash = this.hash;
 
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          window.location.hash = hash;
+            $('html, body').animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    window.location.hash = hash;
+                }
+            );
         }
-      );
-    }
-  });
+    });
 });
 
 // POTVRDA PLAĆANJA
 
-let formTitle = document.querySelector("#title-form");
+let formTitle = document.querySelector('#title-form');
 
 function submitHandler() {
-  document.querySelector("#title-form").innerText = "";
-  return false;
+    document.querySelector('#title-form').innerText = '';
+    return false;
 }
 
 // MOBILNA NAVIGACIJA
 
-const mobileMenuIcon = document.querySelector(".btn-mobile-nav");
+const mobileMenuIcon = document.querySelector('.btn-mobile-nav');
 
-const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector('.navbar');
 
-const navbarItem = document.getElementsByClassName("nav-item");
+const navbarItem = document.getElementsByClassName('nav-item');
 
-mobileMenuIcon.addEventListener("click", function () {
-  navbar.classList.toggle("nav-open");
+mobileMenuIcon.addEventListener('click', function () {
+    navbar.classList.toggle('nav-open');
 });
 
 for (i = 0; i < navbarItem.length; i++) {
-  navbarItem[i].addEventListener("click", function () {
-    navbar.classList.remove("nav-open");
-  });
+    navbarItem[i].addEventListener('click', function () {
+        navbar.classList.remove('nav-open');
+    });
 }
 
 // COUNTER SECTION INCREMENT
@@ -130,12 +130,12 @@ for (i = 0; i < navbarItem.length; i++) {
 
 // TESTIMONIAL SLIDER
 
-const wrapper = document.querySelector(".wrapper");
-const indicators = document.querySelectorAll(".indicator button");
+const wrapper = document.querySelector('.wrapper');
+const indicators = document.querySelectorAll('.indicator button');
 
 let currentTestimonial = 0;
 
-const cards = Array.from(document.querySelectorAll(".review-item.card"));
+const cards = Array.from(document.querySelectorAll('.review-item.card'));
 
 let isDragging = false;
 let startPos = 0;
@@ -144,178 +144,150 @@ let prevTranslate = 0;
 let animationId = 0;
 
 cards.forEach((card, index) => {
-  //Touch
-  card.addEventListener("touchstart", touchStart(index));
-  card.addEventListener("touchend", touchEnd);
-  card.addEventListener("touchmove", touchMove);
+    //Touch
+    card.addEventListener('touchstart', touchStart(index));
+    card.addEventListener('touchend', touchEnd);
+    card.addEventListener('touchmove', touchMove);
 });
 
 function touchStart(index) {
-  return function (event) {
-    startPos = event.touches[0].clientX;
-    isDragging = true;
-    animationId = requestAnimationFrame(animation);
-  };
+    return function (event) {
+        startPos = event.touches[0].clientX;
+        isDragging = true;
+        animationId = requestAnimationFrame(animation);
+    };
 }
 
 function animation() {
-  wrapper.style.marginLeft = `-${100 * currentTestimonial}%`;
+    wrapper.style.marginLeft = `-${100 * currentTestimonial}%`;
 
-  if (isDragging) requestAnimationFrame(animation);
+    if (isDragging) requestAnimationFrame(animation);
 }
 
 function touchEnd() {
-  isDragging = false;
-  cancelAnimationFrame(animationId);
+    isDragging = false;
+    cancelAnimationFrame(animationId);
 
-  const movedBy = currentTranslate - prevTranslate;
-  if (movedBy < -20 && currentTestimonial < cards.length - 1) {
-    currentTestimonial += 1;
-    indicators[currentTestimonial - 1].classList.remove("active-btn");
-    indicators[currentTestimonial].classList.add("active-btn");
-  }
-  if (movedBy > 20 && currentTestimonial > 0) {
-    currentTestimonial -= 1;
-    indicators[currentTestimonial + 1].classList.remove("active-btn");
-    indicators[currentTestimonial].classList.add("active-btn");
-  }
-  setPositionByIndex();
+    const movedBy = currentTranslate - prevTranslate;
+    if (movedBy < -20 && currentTestimonial < cards.length - 1) {
+        currentTestimonial += 1;
+        indicators[currentTestimonial - 1].classList.remove('active-btn');
+        indicators[currentTestimonial].classList.add('active-btn');
+    }
+    if (movedBy > 20 && currentTestimonial > 0) {
+        currentTestimonial -= 1;
+        indicators[currentTestimonial + 1].classList.remove('active-btn');
+        indicators[currentTestimonial].classList.add('active-btn');
+    }
+    setPositionByIndex();
 }
 
 function touchMove(event) {
-  if (isDragging) {
-    const currentPosition = event.touches[0].clientX;
-    currentTranslate = prevTranslate + currentPosition - startPos;
-  }
+    if (isDragging) {
+        const currentPosition = event.touches[0].clientX;
+        currentTranslate = prevTranslate + currentPosition - startPos;
+    }
 }
 
 function setPositionByIndex() {
-  currentTranslate = currentTestimonial * -window.innerWidth;
-  prevTranslate = currentTranslate;
+    currentTranslate = currentTestimonial * -window.innerWidth;
+    prevTranslate = currentTranslate;
 
-  wrapper.style.marginLeft = `-${100 * currentTestimonial}%`;
+    wrapper.style.marginLeft = `-${100 * currentTestimonial}%`;
 }
 
 indicators.forEach((item, i) => {
-  item.addEventListener("click", () => {
-    indicators[currentTestimonial].classList.remove("active-btn");
-    wrapper.style.marginLeft = `-${100 * i}%`;
-    item.classList.add("active-btn");
-    currentTestimonial = i;
-  });
+    item.addEventListener('click', () => {
+        indicators[currentTestimonial].classList.remove('active-btn');
+        wrapper.style.marginLeft = `-${100 * i}%`;
+        item.classList.add('active-btn');
+        currentTestimonial = i;
+    });
 });
 
 // SPINNIG WHEEL
 
 function spinWheel() {
-  const box = document.querySelector("#box");
-  const mainbox = document.querySelector("#mainbox");
-  const message = document.querySelector("#message");
-  const fireworks = document.querySelector(".fireworks");
+    const box = document.querySelector('#box');
+    const mainbox = document.querySelector('#mainbox');
+    const message = document.querySelector('#message');
+    const fireworks = document.querySelector('.fireworks');
 
-  const x = 1024;
-  const y = 5000;
+    const x = 1024;
+    const y = 5000;
 
-  const deg = Math.floor(Math.random() * (x - y) + y);
+    const deg = Math.floor(Math.random() * (x - y) + y);
 
-  // box.style.transform = 'rotate(' + deg + 'deg)';
+    // box.style.transform = 'rotate(' + deg + 'deg)';
 
-  box.style.transform = "rotate(9270deg)";
+    box.style.transform = 'rotate(9270deg)';
 
-  // mainbox.classList.remove('animate');
-  mainbox.classList.add("animate");
-  setTimeout(function () {
-    var valueList = ["10", "20", "50", "100", "150", "200", "400", "500"];
-    var getValue = valueList[Math.floor(Math.random() * valueList.length)];
-    // alert(getValue);
-    message.classList.add("message-animation");
-    fireworks.classList.add("display");
-  }, 8100);
+    // mainbox.classList.remove('animate');
+    mainbox.classList.add('animate');
+    setTimeout(function () {
+        message.classList.add('message-animation');
+        fireworks.classList.add('display');
+    }, 8100);
 
-  setTimeout(function () {
-    fireworks.classList.remove("display");
-  }, 11200);
+    setTimeout(function () {
+        fireworks.classList.remove('display');
+    }, 11200);
 }
 
 // BUYERS NOTIFICATION
 
-const buyers = document.getElementById("buyer");
-const names = [
-  "Ešef",
-  "Suada",
-  "Senad",
-  "Huso",
-  "Kemo",
-  "Nihad",
-  "Jolda",
-  "Neda",
-  "Suljo",
-  "Mujo",
-  "Filka",
-];
+const buyers = document.getElementById('buyer');
 
-const komada = [1, 2, 1, 3, 3, 2, 1, 1, 2, 3, 1];
-const gradovi = [
-  "Srebrenik",
-  "Tuzla",
-  "Jelah",
-  "Sarajevo",
-  "Brčko",
-  "Banja Luka",
-  "Mostar",
-  "Zenica",
-  "Bihać",
-  "Gradiška",
-  "Visoko",
-];
-
-const kupiti = [
-  "Kupio",
-  "Kupila",
-  "Kupio",
-  "Kupio",
-  "Kupio",
-  "Kupio",
-  "Kupio",
-  "Kupila",
-  "Kupio",
-  "Kupio",
-  "Kupila",
-];
-
-const timeAgo = [
-  "Upravo sada",
-  "Prije 1h",
-  "Prije 30 minuta",
-  "Prije 40 minuta",
-  "Upravo sada",
-  "Prije 2h",
-  "Prije 1h",
-  "Upravo sada",
-  "Prije 1h",
-  "Prije 30 minuta",
-  "Prije 40 minuta",
+const buyerSlider = [
+    {
+        name: 'Sanjin',
+        komada: 1,
+        grad: 'Srebrenik',
+        kupiti: 'Kupio',
+        vrijeme: 'Upravo sada',
+    },
+    {
+        name: 'Suada',
+        komada: 2,
+        grad: 'Tuzla',
+        kupiti: 'Kupila',
+        vrijeme: 'Prije 20 sekundi',
+    },
+    {
+        name: 'Fadila',
+        komada: 2,
+        grad: 'Visoko',
+        kupiti: 'Kupila',
+        vrijeme: 'Prije 10 sekundi',
+    },
+    {
+        name: 'Zoran',
+        komada: 1,
+        grad: 'Zvornik',
+        kupiti: 'Kupio',
+        vrijeme: 'Prije 40 sekundi',
+    },
 ];
 
 let ind = 0;
-const len = names.length;
+const len = buyerSlider.length;
 
 setInterval(function () {
-  buyers.style.transform = "translateX(0)";
-  buyers.classList.remove("transparent");
+    buyers.style.transform = 'translateX(0)';
+    buyers.classList.remove('transparent');
 
-  buyers.innerHTML = `<div class="cont">
+    buyers.innerHTML = `<div class="cont">
                 <div class="image">
                     <i class="far fa-thumbs-up"></i>
                 </div> 
                 <div class="buyers-box">
-                <div class="name">${names[ind]} <span>, ${
-    gradovi[ind]
-  }</span></div>
-                <div class="desc">${kupiti[ind]} ${komada[ind]} ${
-    komada[ind] == 1 ? "kutiju" : "kutije"
-  } Natura Active</div>
-                <small>${timeAgo[ind]}</small>
+                <div class="name">${buyerSlider[ind].name} <span>, ${
+        buyerSlider[ind].grad
+    }</span></div>
+                <div class="desc">${buyerSlider[ind].kupiti} ${
+        buyerSlider[ind].komada
+    } ${buyerSlider[ind].komada == 1 ? 'kutiju' : 'kutije'} Natura Active</div>
+                <small>${buyerSlider[ind].vrijeme}</small>
                 </div>
                 <div class="image">
                             <img
@@ -325,15 +297,15 @@ setInterval(function () {
                         </div>
             </div>`;
 
-  if (ind < len) {
-    ind++;
-  } else {
-    ind = 1;
-  }
+    if (ind < len) {
+        ind++;
+    } else {
+        ind = 1;
+    }
 }, 8000);
 
 setInterval(function () {
-  buyers.classList.add("transparent");
-  buyers.style.transform = "translateX(-200%)";
-  ind--;
+    buyers.classList.add('transparent');
+    buyers.style.transform = 'translateX(-200%)';
+    ind--;
 }, 16000);
